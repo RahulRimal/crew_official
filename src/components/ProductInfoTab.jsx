9972727cd0094f0c8f7a11ef0846122ae9910de7
Rahul Rimal <genuinetechnology9@gmail.com>
2022-10-25 15:45:49 +0545

    Added animation to product description section

--- src/components/ProductInfoTab.jsx
+++ src/components/ProductInfoTab.jsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import React, { useState } from "react";
 import styled from "styled-components";
 
@@ -17,109 +18,190 @@ const ProductInfoTab = ({ product }) => {
             className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
             onClick={() => toggleTab(1)}
           >
+            {toggleState === 1 ? (
+              <motion.div className="underline" layoutId="underline" />
+            ) : null}
             Prouct Description
           </button>
           <button
             className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
             onClick={() => toggleTab(2)}
           >
+            {toggleState === 2 ? (
+              <motion.div className="underline" layoutId="underline" />
+            ) : null}
             Terms &amp; Conditions
           </button>
         </div>
 
         <div className="content-tabs">
-          <div
-            className={
-              toggleState === 1 ? "content  active-content" : "content"
-            }
-          >
-            <p>{description || "No description available for this product"}</p>
-            {technical_specification.length > 0 && (
-              <h2>Technical Specifications</h2>
+          <AnimatePresence>
+            {toggleState === 1 && (
+              <motion.div
+                initial={{
+                  height: 0,
+                  opacity: 0,
+                }}
+                animate={{
+                  height: "auto",
+                  opacity: 1,
+                  transition: {
+                    height: {
+                      duration: 0.4,
+                    },
+                    opacity: {
+                      duration: 0.25,
+                      delay: 0.15,
+                    },
+                  },
+                }}
+                exit={{
+                  height: 0,
+                  opacity: 0,
+                  transition: {
+                    height: {
+                      duration: 0.4,
+                    },
+                    opacity: {
+                      duration: 0.25,
+                    },
+                  },
+                }}
+              >
+                <div
+                  className={
+                    toggleState === 1 ? "content  active-content" : "content"
+                  }
+                >
+                  <p>
+                    {description || "No description available for this product"}
+                  </p>
+                  {technical_specification.length > 0 && (
+                    <h2>Technical Specifications</h2>
+                  )}
+                  <ul>
+                    {technical_specification.map((spec, idx) => {
+                      const { specification } = spec;
+                      return (
+                        <li key={idx}>
+                          {" "}
+                          <p>{specification}</p>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </motion.div>
             )}
-            <ul>
-              {technical_specification.map((spec, idx) => {
-                const { specification } = spec;
-                return (
-                  <li key={idx}>
-                    {" "}
-                    <p>{specification}</p>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+          </AnimatePresence>
 
-          <div
-            className={
-              toggleState === 2 ? "content  active-content" : "content"
-            }
-          >
-            <p>
-              <strong>Site - Pickup: </strong> This Product is available for
-              Site - Pickup as well as Site - Delivery.
-            </p>
-            <br />
-            <p>
-              <strong>For Delivery: </strong> You need to book the product 2
-              days prior &amp; inform us, whereas Delivery Charges is extra
-              based on your location from our location. Actual Address of the
-              site-pickup will be mentioned on the Invoice Received after
-              booking
-            </p>
-            <br />
-            <p>
-              <strong>Documents Required &amp; ID Verification</strong>. Anyone
-              Valid Govt ID is mandatory We will take Post Dated Cheque of the
-              replacement value of the product.
-            </p>
-            <br />
-            <ul>
-              <strong> Valid Documents: </strong>
-              <li>Voter Card</li>
-              <li> Driving License</li>
-              {/* <li> Passport (No Aadhar card required)</li> */}
-              <li>Local Residence Proof</li>
-            </ul>
-            <br />
-            <p>
-              <strong> Security Deposit: </strong>We have No Cash Security
-              Policy, instead, we will take a cheque and a valid Govt ID. We
-              will take a <strong> Post Dated Cheque</strong> of the Replacement
-              Value of the Product and a Valid ID. Security deposit does not
-              include rent. It takes care of damages, loss or mishandling (if
-              any). Post Dated Cheque will be handed over to you as soon as you
-              return the product (without Damage).
-            </p>
-            <br />
-            <p>
-              <strong> Bookings, Cancellation &amp; Refunds:</strong> For
-              Confirmed Booking and Holding a rented equipment, Booking amount
-              mentioned must be paid, the rest amount will be paid in advance at
-              the time of site-pickup or delivery.
-            </p>
-            <br />
-            <br />
-            <ul>
-              Canceling a product is liable to some charges as mentioned:
-              <li>
-                {" "}
-                <strong> 7 Days Prior:</strong> 10% of Total Rent
-              </li>
-              <li>
-                <strong> 2 Days Prior:</strong> 20% of Total Rent
-              </li>{" "}
-              <li>
-                {" "}
-                <strong> 1 Day Prior: </strong> 30% of Total Rent{" "}
-              </li>
-            </ul>
-            <br />
-            <p>
-              Any refunds related issues will be solved within 7-10 Working Days
-              after confirmation of Refund.
-            </p>
-          </div>
+          <AnimatePresence>
+            {toggleState === 2 && (
+              <motion.div
+                initial={{
+                  height: 0,
+                  opacity: 0,
+                }}
+                animate={{
+                  height: "auto",
+                  opacity: 1,
+                  transition: {
+                    height: {
+                      duration: 0.4,
+                    },
+                    opacity: {
+                      duration: 0.25,
+                      delay: 0.15,
+                    },
+                  },
+                }}
+                exit={{
+                  height: 0,
+                  opacity: 0,
+                  transition: {
+                    height: {
+                      duration: 0.4,
+                    },
+                    opacity: {
+                      duration: 0.25,
+                    },
+                  },
+                }}
+              >
+                <div
+                  className={
+                    toggleState === 2 ? "content  active-content" : "content"
+                  }
+                >
+                  <p>
+                    <strong>Site - Pickup: </strong> This Product is available
+                    for Site - Pickup as well as Site - Delivery.
+                  </p>
+                  <br />
+                  <p>
+                    <strong>For Delivery: </strong> You need to book the product
+                    2 days prior &amp; inform us, whereas Delivery Charges is
+                    extra based on your location from our location. Actual
+                    Address of the site-pickup will be mentioned on the Invoice
+                    Received after booking
+                  </p>
+                  <br />
+                  <p>
+                    <strong>Documents Required &amp; ID Verification</strong>.
+                    Anyone Valid Govt ID is mandatory We will take Post Dated
+                    Cheque of the replacement value of the product.
+                  </p>
+                  <br />
+                  <ul>
+                    <strong> Valid Documents: </strong>
+                    <li>Voter Card</li>
+                    <li> Driving License</li>
+                    {/* <li> Passport (No Aadhar card required)</li> */}
+                    <li>Local Residence Proof</li>
+                  </ul>
+                  <br />
+                  <p>
+                    <strong> Security Deposit: </strong>We have No Cash Security
+                    Policy, instead, we will take a cheque and a valid Govt ID.
+                    We will take a <strong> Post Dated Cheque</strong> of the
+                    Replacement Value of the Product and a Valid ID. Security
+                    deposit does not include rent. It takes care of damages,
+                    loss or mishandling (if any). Post Dated Cheque will be
+                    handed over to you as soon as you return the product
+                    (without Damage).
+                  </p>
+                  <br />
+                  <p>
+                    <strong> Bookings, Cancellation &amp; Refunds:</strong> For
+                    Confirmed Booking and Holding a rented equipment, Booking
+                    amount mentioned must be paid, the rest amount will be paid
+                    in advance at the time of site-pickup or delivery.
+                  </p>
+                  <br />
+                  <br />
+                  <ul>
+                    Canceling a product is liable to some charges as mentioned:
+                    <li>
+                      {" "}
+                      <strong> 7 Days Prior:</strong> 10% of Total Rent
+                    </li>
+                    <li>
+                      <strong> 2 Days Prior:</strong> 20% of Total Rent
+                    </li>{" "}
+                    <li>
+                      {" "}
+                      <strong> 1 Day Prior: </strong> 30% of Total Rent{" "}
+                    </li>
+                  </ul>
+                  <br />
+                  <p>
+                    Any refunds related issues will be solved within 7-10
+                    Working Days after confirmation of Refund.
+                  </p>
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </div>
     </Wrapper>
@@ -155,6 +237,7 @@ const Wrapper = styled.div`
     outline: none;
     font-size: 1.6rem;
     font-weight: 500;
+    position: relative;
   }
   .tabs:not(:last-child) {
     border-right: 1px solid rgba(0, 0, 0, 0.274);
@@ -168,13 +251,22 @@ const Wrapper = styled.div`
     font-weight: 500;
   }
 
-  .active-tabs::before {
+  /* .active-tabs::before {
     content: "";
     display: block;
     position: absolute;
     top: -5px;
     left: 50%;
     transform: translateX(-50%);
+    width: calc(100% + 2px);
+    height: 5px;
+    background: var(--primary-color);
+  } */
+  .underline {
+    position: absolute;
+    top: -5px;
+    left: 0;
+    right: 0;
     width: calc(100% + 2px);
     height: 5px;
     background: var(--primary-color);
