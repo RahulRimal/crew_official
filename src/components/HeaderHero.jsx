d0ad50f5e22e0401433bdb2f9dea0570e94a3106
Rahul Rimal <genuinetechnology9@gmail.com>
2022-10-26 16:00:30 +0545

    Added content to header hero

--- src/components/HeaderHero.jsx
+++ src/components/HeaderHero.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import { MdOutlineArrowRightAlt } from "react-icons/md";
+import { Link } from "react-router-dom";
 import Slider from "react-slick";
 import styled from "styled-components";
 
 const HeaderHero = () => {
+  let heroSlider = null;
+
   const herolidersettings = {
     // dots: false,
     infinite: true,
-    // autoplay: true,
+    autoplay: true,
     fade: true,
     speed: 4000,
-    autoplaySpeed: 8000,
+    autoplaySpeed: 5000,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
+    pauseOnHover: false,
   };
 
   const clientSlidersettings = {
@@ -37,35 +42,127 @@ const HeaderHero = () => {
 
   return (
     <Wrapper>
-      <div className="overlay"></div>
-      <Slider {...herolidersettings} className="hero-carousel">
+      {/* <div className="overlay"></div> */}
+      <Slider
+        ref={(slider) => (heroSlider = slider)}
+        {...herolidersettings}
+        className="hero-carousel"
+      >
         <div className="hero-carousel-item">
+          <div className="overlay"></div>
           <img
             src="https://source.unsplash.com/3200x1200/?nature,mountain"
             alt="Carousel content"
           />
           <div className="carousel-content">
-            <p>
+            <p className="title">Lorem, ipsum. Babal Text</p>
+            <p className="desc">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
               rerum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Commodi quidem deleniti sequi amet enim eum porro impedit sint
               praesentium nulla.
             </p>
-            <button className="carousel-cta">Rent Now</button>
+            <Link
+              to="#"
+              className="carousel-cta"
+              onMouseOver={() => heroSlider.slickPause()}
+              onMouseLeave={() => heroSlider.slickPlay()}
+            >
+              Rent now <MdOutlineArrowRightAlt />
+            </Link>
           </div>
         </div>
-        <img
-          src="https://source.unsplash.com/3200x1200/?nature,city"
-          alt="Carousel content"
-        />
-        <img
-          src="https://source.unsplash.com/3200x1200/?stream"
-          alt="Carousel content"
-        />
-        <img
-          src="https://source.unsplash.com/3200x1200/?nepal"
-          alt="Carousel content"
-        />
+        <div className="hero-carousel-item">
+          <div className="overlay"></div>
+          <img
+            src="https://source.unsplash.com/3200x1200/?nature,people"
+            alt="Carousel content"
+          />
+          <div className="carousel-content">
+            <p className="title">Lorem, ipsum. Babal Text</p>
+            <p className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. In,
+              similique.
+            </p>
+            <Link
+              to="#"
+              className="carousel-cta"
+              onMouseOver={() => heroSlider.slickPause()}
+              onMouseLeave={() => heroSlider.slickPlay()}
+            >
+              Rent now <MdOutlineArrowRightAlt />
+            </Link>
+          </div>
+        </div>
+        <div className="hero-carousel-item">
+          <div className="overlay"></div>
+          <img
+            src="https://source.unsplash.com/3200x1200/?lion,water"
+            alt="Carousel content"
+          />
+          <div className="carousel-content">
+            <p className="title">Lorem, ipsum. Babal Text</p>
+            <p className="desc">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Recusandae, ipsa eius dolores id aliquam et.
+            </p>
+
+            <Link
+              to="#"
+              className="carousel-cta"
+              onMouseOver={() => heroSlider.slickPause()}
+              onMouseLeave={() => heroSlider.slickPlay()}
+            >
+              Rent now <MdOutlineArrowRightAlt />
+            </Link>
+          </div>
+        </div>
+        <div className="hero-carousel-item">
+          <div className="overlay"></div>
+          <img
+            src="https://source.unsplash.com/3200x1200/?cat,mountain"
+            alt="Carousel content"
+          />
+          <div className="carousel-content">
+            <p className="title">Lorem, ipsum. Babal Text</p>
+            <p className="desc">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos qui
+              temporibus cum laborum iste quam eius in iure sint delectus!
+            </p>
+            <Link
+              to="#"
+              className="carousel-cta"
+              onMouseOver={() => heroSlider.slickPause()}
+              onMouseLeave={() => heroSlider.slickPlay()}
+            >
+              Rent now <MdOutlineArrowRightAlt />
+            </Link>
+          </div>
+        </div>
+        <div className="hero-carousel-item">
+          <div className="overlay"></div>
+          <img
+            src="https://source.unsplash.com/3200x1200/?city,people"
+            alt="Carousel content"
+          />
+          <div className="carousel-content">
+            <p className="title">Lorem, ipsum. Babal Text</p>
+            <p className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
+              rerum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Commodi quidem deleniti sequi amet enim eum porro impedit sint
+              praesentium nulla.
+            </p>
+            <Link
+              to="#"
+              className="carousel-cta"
+              onMouseOver={() => heroSlider.slickPause()}
+              onMouseLeave={() => heroSlider.slickPlay()}
+            >
+              Rent now <MdOutlineArrowRightAlt />
+            </Link>
+          </div>
+        </div>
       </Slider>
 
       <div className="hero-clients">
@@ -136,27 +233,74 @@ const Wrapper = styled.div`
   height: 600px;
   /* height: 100vh; */
 
-  .overlay {
+  /* .overlay {
     position: absolute;
     width: 100%;
     height: 100%;
     z-index: 97;
     background: rgba(0, 0, 0, 0.2);
-  }
+  } */
 
   .hero-carousel {
-    .slick-slide > div {
+    /* .slick-slide > div {
       height: 600px;
-    }
+    } */
 
     .hero-carousel-item {
+      position: relative;
+      height: 600px;
+
+      .overlay {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        z-index: 97;
+        background: rgba(0, 0, 0, 0.3);
+      }
+
       .carousel-content {
         position: absolute;
         bottom: 152px;
         left: 200px;
-        color: var(--primary-white);
         width: 450px;
         z-index: 99;
+      }
+      p {
+        color: var(--primary-white);
+        font-family: "Roboto", sans-serif;
+        letter-spacing: 1px;
+      }
+
+      .title {
+        font-size: 4.4rem;
+        font-weight: bold;
+        margin: 2rem 0;
+      }
+
+      .desc {
+        line-height: 2rem;
+      }
+
+      .carousel-cta {
+        /* display: flex; */
+        display: inline-flex;
+        justify-content: space-between;
+        align-items: center;
+        border: none;
+        background-color: var(--primary-color);
+        color: var(--primary-white);
+        padding: 10px 20px;
+        border-radius: 5px;
+        margin-top: 1.2rem;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        font-weight: 600;
+        cursor: pointer;
+        z-index: 99;
+
+        svg {
+          font-size: 2rem;
+        }
       }
     }
   }
@@ -170,7 +314,7 @@ const Wrapper = styled.div`
     position: absolute;
     bottom: 0;
     right: 0;
-    background: rgba(0, 0, 0, 0.2);
+    background: rgba(0, 0, 0, 0.4);
     height: 550px;
     width: 350px;
     border-top-left-radius: 10px;
